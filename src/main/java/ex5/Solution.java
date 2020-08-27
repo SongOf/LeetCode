b1ed30ef9ed8ad3c4299068138d4b1754705aa92
SongOf <1434749602@qq.com>
2020-08-28 00:54:45 +0800

    thr

--- src/main/java/ex5/Solution.java
+++ src/main/java/ex5/Solution.java
@@ -1,34 +1,48 @@
 package ex5;
-
+//中心扩展法
 public class Solution {
     public String longestPalindrome(String s) {
-        if(s.length()==1||s.length()==0){
+        int sLength=s.length();
+        if(sLength<2){
             return s;
         }
+
         int maxLength=0;
-        String res="";
-        int j=0;
-        String reverseSubStr="";
-        String subStr="";
-        for(int i=0;i<s.length()-1;i++){
-            reverseSubStr="";
-            subStr="";
-            subStr=subStr+s.charAt(i);
-            reverseSubStr=reverseSubStr+s.charAt(i);
-            for(j=i;j<s.length();){
-                if(subStr.equals(reverseSubStr) && maxLength<(j-i+1)){
-                    maxLength=j-i+1;
-                    res=subStr;
-                }
-                j++;
-                subStr=subStr+s.charAt(j);
-                reverseSubStr=s.charAt(j)+reverseSubStr;
+        int[] resLoc=null;
+        int[] resLocTemp=null;
+
+        int len=0;
+        for(int i=0;i<sLength;i++){
+            resLocTemp=this.centerExpand(s,i,i);
+            len=resLocTemp[1]-resLocTemp[0]+1;
+            if(maxLength<len){
+                maxLength=len;
+                resLoc=resLocTemp;
+            }
+            resLocTemp=this.centerExpand(s,i,i+1);
+            len=resLocTemp[1]-resLocTemp[0]+1;
+            if(maxLength<len){
+                maxLength=len;
+                resLoc=resLocTemp;
             }
         }
-        return res;
+        return s.substring(resLoc[0],resLoc[1]+1);
+    }
+    public int[] centerExpand(String s,int left,int right){
+        int[] resLoc=new int[2];
+        int sLength=s.length();
+        while(left>=0 && right<sLength && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        left++;
+        right--;
+        resLoc[0]=left;
+        resLoc[1]=right;
+        return resLoc;
     }
     public static void main(String[] args){
-        String s="ac";
+        String s="babad";
         Solution solution = new Solution();
         String res= solution.longestPalindrome(s);
         System.out.println(res);
